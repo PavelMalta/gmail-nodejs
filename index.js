130d5edddf1c5686c1d9cdc200b7b4d545ea4e90
PavelMalta <shamashov87@mail.ru>
2021-06-29 15:50:07 +0300

    update cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
