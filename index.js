c05e78866de5715134d29f52ac690b25ac92e06e
PavelMalta <shamashov87@mail.ru>
2021-08-22 21:54:11 +0300

    update cors argument

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.options("/*", function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.sendStatus(200);
+});
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
 app.use(cors({origin: "https://PavelMalta.github.io/Portfolio"}));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
